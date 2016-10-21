28c774bd6508c1c47f0e48119e8d0c5690662ce2
tangjonathan <jontang92@gmail.com>
2016-10-20 21:16:29 -0400

    Add User authentication. Cleaned up controllers

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 var express = require('express');
+var session = require('express-session');
 var app = express();
+var flash = require('connect-flash');
+var passport = require('passport');
 var mongoose = require('mongoose');
 var config = require('./config');
-var quizController = require('./controllers/quiz');
-var userController = require('./controllers/user');
+var quizController = require('./controllers/quizController');
+var userController = require('./controllers/userController');
+var sessionController = require('./controllers/sessionController');
+
+require('./config/passport')(passport);
 
 var port = process.env.PORT || 3000;
 
 app.use('/assets', express.static(__dirname + '/public'));
+
+app.use(session({secret:'mySession'}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
 app.set('view engine', 'ejs');
 
 mongoose.connect(config.getDbConnectionString());
 quizController(app);
 userController(app);
+sessionController(app);
 
 app.listen(port);
--- models/user.js
+++ models/user.js
@@ -1,12 +1,20 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
 
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
     username: String,
     password: String,
-    id: String
 });
+
+userSchema.methods.generateHash = function(password){
+return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+userSchema.methods.validPassword = function(password){
+    return bcrypt.compareSync(password, this.password);
+};
 
 var User = mongoose.model('User', userSchema);
 
